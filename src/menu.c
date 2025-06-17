42c13ddb2fe3d58850e74bd09d319e20a6d07299
silicat <silicatmeow@gmail.com>
2025-06-16 20:44:47 -0400

    Partially implement greyscale support and add Evangelion demo

--- src/menu.c
+++ src/menu.c
@@ -1,6 +1,7 @@
 #include <tigcclib.h>
 
 #include "menu.h"
+#include "gray.h"
 
 /*
   draw_menu_border (x, y, w, h)
@@ -53,6 +54,8 @@ inline void DrawSimpleMenuBorder(uint8_t x, uint8_t y, uint8_t w, uint8_t h)
  * computing the mtx transpose just in time would save a little memory if we have many borders.
  * maybe we could draw big boxes with sprite16/32, somehow memcpying and then drawing with sprite16/32
  * not sure if it would be much faster, id have to benchmark it.
+ *
+ * TODO ... rename DrawMenuBox? or maybe we separate bkgd drawing...? we'll see.
  */
 void DrawMenuBorder(border_pattern_t *pattern, uint8_t x, uint8_t y, uint8_t w, uint8_t h)
 {
@@ -61,22 +64,40 @@ void DrawMenuBorder(border_pattern_t *pattern, uint8_t x, uint8_t y, uint8_t w, 
      return;
   }
 
-  /* draw corners first */
-  Sprite8(x, y,         pattern->height, pattern->corners.split.corners_tl, LCD_MEM, SPRT_XOR); // top left
-  Sprite8(x + w, y,     pattern->height, pattern->corners.split.corners_tr, LCD_MEM, SPRT_XOR); // top right
-  Sprite8(x, y + h,     pattern->height, pattern->corners.split.corners_bl, LCD_MEM, SPRT_XOR); // bot left
-  Sprite8(x + w, y + h, pattern->height, pattern->corners.split.corners_br, LCD_MEM, SPRT_XOR); // bot right
+  /* 
+    small bug in the clear routine. x + w doesnt necessarily mean the end of the box.
+    im not sure if its overrun to say x+w+pat_width or if thats perfect for all cases.
+    either way this works, and we have vmem to overrun anyway if its buggy... lol.
+  */
 
-  /* draw the top&bottom bars */
+  /* (slow) clear routine */
+  SetPlane (DARK_PLANE);
+  ScrRectFill (&(SCR_RECT){{x,y,x+w+pattern->width,y+h}}, &(SCR_RECT){{0, 0,160, 100}}, A_REVERSE);
+  SetPlane (LIGHT_PLANE);
+  ScrRectFill (&(SCR_RECT){{x,y,x+w+pattern->width,y+h}}, &(SCR_RECT){{0, 0,160, 100}}, A_REVERSE);
+
+  /* draw corners first */
+  Sprite8(x, y,         pattern->height, pattern->corners.split.corners_tl, GetPlane(DARK_PLANE), SPRT_XOR); // top left
+  Sprite8(x + w, y,     pattern->height, pattern->corners.split.corners_tr, GetPlane(DARK_PLANE), SPRT_XOR); // top right
+  Sprite8(x, y + h,     pattern->height, pattern->corners.split.corners_bl, GetPlane(DARK_PLANE), SPRT_XOR); // bot left
+  Sprite8(x + w, y + h, pattern->height, pattern->corners.split.corners_br, GetPlane(DARK_PLANE), SPRT_XOR); // bot right
+
+  /* draw the top and bottom bars */
   for(uint8_t i = pattern->width; i <= w - pattern->width; i += pattern->width) {
-    /* draw on the top and bottom */
-    Sprite8(x + i, y,     pattern->height, pattern->pattern.split.pattern_h, LCD_MEM, SPRT_XOR);
-    Sprite8(x + i, y + h, pattern->height, pattern->pattern.split.pattern_h, LCD_MEM, SPRT_XOR);
+    Sprite8(x + i, y,     pattern->height, pattern->pattern.split.pattern_h, GetPlane(DARK_PLANE), SPRT_XOR);
+    Sprite8(x + i, y + h, pattern->height, pattern->pattern.split.pattern_h, GetPlane(DARK_PLANE), SPRT_XOR);
   }
 
   /* draw the left and right bars */
   for(uint8_t i = pattern->height; i <= h - pattern->height; i += pattern->height) {
-    Sprite8(x,   y + i, pattern->height, pattern->pattern.split.pattern_v, LCD_MEM, SPRT_XOR);
-    Sprite8(x+w, y + i, pattern->height, pattern->pattern.split.pattern_v, LCD_MEM, SPRT_XOR); 
+    Sprite8(x,   y + i, pattern->height, pattern->pattern.split.pattern_v, GetPlane(DARK_PLANE), SPRT_XOR);
+    Sprite8(x+w, y + i, pattern->height, pattern->pattern.split.pattern_v, GetPlane(DARK_PLANE), SPRT_XOR); 
   }
+
+  /*
+    also, TODO: this will only draw on one plane (for the sake of speed)
+    but what if we want greyscale borders (2x the memory womp womp) or if
+    we just want dark borders? (gotta run the whole thing again with GetPlane(LIGHT_PLANE))!
+  */
+
 }
